f9bba35fceae2f8c3cf9e224b6c3a2abb5990267
Peter Klassen <klassen@hiskp.uni-bonn.de>
2023-05-08 10:07:35 +0200

    publich HA config message

--- src/mqtt_client.h
+++ src/mqtt_client.h
@@ -6,6 +6,32 @@
 
 PubSubClient mqtt(wifiClient);
 
+bool mqttPublishConfig(REMOTE *r)
+{
+    char payLoad[500];
+    snprintf(payLoad, sizeof(payLoad),
+             "{"
+             "\"~\":\"%s\","
+             "\"name\":\"MQTT Cover %s\","
+             "\"uniq_id\":\"%X\","
+             "\"cmd_t\":\"~/set\","
+             "\"dev_cla\":\"shutter\","
+             "\"qos\":0,"
+             "\"ret\":true,"
+             "\"pl_open\":\"u\","
+             "\"pl_cls\":\"d\","
+             "\"pl_stop\":\"s\","
+             "\"avty_t\":\"%s\","
+             "\"pl_avail\":\"online\","
+             "\"pl_not_avail\":\"offline\","
+             "\"opt\":true"
+             "}",
+             r->getMQTT_topic(""),
+             r->getMQTT_name(),
+             r->getId(),
+             status_topic);
+    return mqtt.publish(r->getMQTT_topic("config"), payLoad);
+}
 
 void mqttconnect()
 {
@@ -13,6 +39,9 @@ void mqttconnect()
     while (!mqtt.connected())
     {
         Serial.print("MQTT connecting ...");
+
+        if (mqtt.setBufferSize(500))
+            Serial.print("buffer size increased");
 
         // Connect to MQTT, with retained last will message "offline"
         if (mqtt.connect(mqtt_id, mqtt_user, mqtt_password, status_topic, 1, 1, "offline"))
@@ -26,6 +55,9 @@ void mqttconnect()
                 mqtt.subscribe(remote->getMQTT_topic("set"), 1);
                 Serial.print("Subscribed to topic: ");
                 Serial.println(remote->getMQTT_topic("set"));
+
+                if (!mqttPublishConfig(remote))
+                    Serial.println("pub failed, mqtt buffer too small?");
             }
 
             // Update status, message is retained
